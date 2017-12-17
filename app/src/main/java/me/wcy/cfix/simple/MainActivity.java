db4510dd3775d7ed13e9b0a1f18e2406123583e6
wcy <919247701@qq.com>
2017-12-17 12:13:49 +0800

    update

--- app/src/main/java/me/wcy/cfix/simple/MainActivity.java
+++ app/src/main/java/me/wcy/cfix/simple/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FixDialog.get().show(this);
+        //FixDialog.get().show(this);
     }
 }
